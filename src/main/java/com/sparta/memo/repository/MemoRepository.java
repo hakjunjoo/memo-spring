c8ffcd609c7925c7b0756f4c05a3fe08e1fac558
hakjunjoo <hakjun0918@nate.com>
2023-06-14 16:16:45 +0900

    JPA Auditing

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -2,7 +2,6 @@ package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
